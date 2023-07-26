5eeb0782037ddf4c4fb7a0e202e96ea33680160b
Joy Burke <joyaburke17@gmail.com>
2023-07-26 13:09:59 -0700

    thunked-partners

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AboutPage from "./pages/AboutPage";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import { fetchPartners } from "./features/partners/partnersSlice";
 import "./App.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
   }, [dispatch]);
 
   return (
--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,10 +1,32 @@
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
 import { useSelector } from "react-redux";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const PartnersList = () => {
   const partners = useSelector(selectAllPartners);
+
+  const isLoading = useSelector((state) => state.partners.isLoading);
+  const errMsg = useSelector((state) => state.partners.errMsg);
+
+  if (isLoading) {
+    return (
+      <Row>
+        <Loading />
+      </Row>
+    );
+  }
+
+  if (errMsg) {
+    return (
+      <Row>
+        <Error errMsg={errMsg} />
+      </Row>
+    );
+  }
+
   return (
     <Col className="mt-4">
       {partners.map((partner) => {
